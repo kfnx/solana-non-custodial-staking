5f3d8a07b2c220fa70a64477297d65e8d4bbd818
kafinsalim <kafinsalim@gmail.com>
2022-04-28 19:40:04 +0700

    revert prog id

--- programs/nc-staking/src/lib.rs
+++ programs/nc-staking/src/lib.rs
@@ -4,7 +4,7 @@ use anchor_spl::token::{self, Approve, Mint, Token, TokenAccount};
 mod error;
 mod mpl;
 
-declare_id!("213MTwSbPbBzi8jH7xJ5aFC56j6DCnsAaBHFz98NNe8s");
+declare_id!("stakEUMMv9bRHYX4CyVY48i19ViBdNSzn8Rt1a1Fi6E");
 
 const DELEGATE_PDA_SEED: &[u8] = b"delegate";
 
