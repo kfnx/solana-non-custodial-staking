ad65b04ff76524ec829e37f10d1bd3d820cbfe37
kafinsalim <kafinsalim@gmail.com>
2022-05-11 01:41:12 +0700

    add localnet test with an NFT to test staking

--- custom-types.d.ts
+++ custom-types.d.ts
@@ -169,4 +169,25 @@ declare module "@solana/spl-token" {
     programId?: PublicKey,
     associatedTokenProgramId?: PublicKey
   ): Promise<PublicKey>;
+
+  /**
+   * Construct an AssociatedTokenAccount instruction
+   *
+   * @param payer                    Payer of the initialization fees
+   * @param associatedToken          New associated token account
+   * @param owner                    Owner of the new account
+   * @param mint                     Token mint account
+   * @param programId                SPL Token program account
+   * @param associatedTokenProgramId SPL Associated Token program account
+   *
+   * @return Instruction to add to a transaction
+   */
+  export function createAssociatedTokenAccountInstruction(
+    payer: PublicKey,
+    associatedToken: PublicKey,
+    owner: PublicKey,
+    mint: PublicKey,
+    programId?: PublicKey,
+    associatedTokenProgramId?: PublicKey
+  ): TransactionInstruction;
 }
--- tests/utils/index.ts
+++ tests/utils/index.ts
@@ -1,6 +1,7 @@
 export * from "./pda";
 export * from "./user";
 export * from "./program-id";
+export * from "./metaplex";
 
 export async function delay(ms: number) {
   await new Promise((response) =>
